2b595cc6a9abdc051e79eb10773506f39a73f1ce
its-samwell <adetunji.samuel@rocketmail.com>
2019-06-15 13:41:54 +0100

    changes

--- api/routes/Users.js
+++ api/routes/Users.js
@@ -47,6 +47,7 @@ router.post('/',(req,res)=>{
                                             type : req.body.type
 
                                         });
+                                        //remove password
                                     Newuser.save().then((output)=>{res.json({output});})
                                         .catch((err)=>{res.json({ success: "false", Error: Perrors.Unknown });});
                                 }
@@ -62,4 +63,47 @@ router.post('/',(req,res)=>{
     }
 });
 
+
+//@route Post /user/login
+//@desc to login
+//@access public
+
+
+router.post('/login',(req,res)=>{
+    if(!req.body.email || req.body.password)
+    {
+        res.json({success:"false",msg:pe.Required});
+    }
+    else
+    {
+        User.findOne({email:req.body.email},(err,rdata)=>{
+            if(err||!rdata) 
+            {
+                res.json({success:"false",msg:"Invalid"});
+            }
+            else
+            {
+                bcrypt.compare(req.body.password,rdata.password, function(err,isvalid) {
+                    // res == true
+                    if(err || isvalid==false){
+                        res.json({success:"false",msg:"Auth failed"});
+                    }
+                    else{
+                        jwt.sign({id:rdata._id,email:rdata.email,fname:rdata.fname,created :Date.now()},privatekey,{expiresIn:"10h"},(err,token)=>{
+                            if(err)
+                            {
+                                res.json({success:"false",msg:"Something went wrong"});
+                            }
+                            else{
+                                res.json({success:"true",token:"Bearer "+token});
+                            }
+                        });
+                    }
+                });
+            }
+        });
+            
+    }
+    
+});
 module.exports = router;
